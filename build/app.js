cb82b09e197c0842b64b50f9ff9789d16903faf9
cam <cameron@catchdesign.co.nz>
2022-10-26 22:24:17 +1300

    custom reducer event wip

--- build/app.js
+++ build/app.js
@@ -1,3 +1,4 @@
+import { replaceHTML } from "./helpers.js";
 export var Page;
 (function (Page) {
     Page["All"] = "all";
@@ -16,13 +17,13 @@ export var Message;
 export function generateDefaultState() {
     return {
         todos: [],
+        idLedger: 1,
         page: Page.All,
     };
 }
-let state = generateDefaultState();
 export function update(state, message, payload) {
     var _a, _b, _c;
-    const id = (_a = payload === null || payload === void 0 ? void 0 : payload.todoID) !== null && _a !== void 0 ? _a : "";
+    const id = (_a = payload === null || payload === void 0 ? void 0 : payload.todoID) !== null && _a !== void 0 ? _a : 0;
     const title = (_b = payload === null || payload === void 0 ? void 0 : payload.todoTitle) !== null && _b !== void 0 ? _b : "";
     const page = (_c = payload === null || payload === void 0 ? void 0 : payload.page) !== null && _c !== void 0 ? _c : "";
     if (message === Message.AddTodo) {
@@ -32,9 +33,10 @@ export function update(state, message, payload) {
         const todo = {
             title: title,
             completed: false,
-            id: "id_" + Date.now(),
+            id: state.idLedger || 1,
         };
         state.todos.push(todo);
+        state.idLedger += 1;
     }
     if (message === Message.RemoveTodo) {
         if (!id) {
@@ -66,4 +68,62 @@ export function update(state, message, payload) {
     const updated = Object.assign(Object.assign({}, state), { todos: [...state.todos] });
     return updated;
 }
+function toggleVisible(el, show) {
+    el.style.display = show ? "block" : "none";
+}
+export function render(state, domElements) {
+    const hasTodos = !!state.todos.length;
+    const hasCompletedTodos = state.todos.some((t) => t.completed);
+    const isAllTodosCompleted = state.todos.every((todo) => todo.completed);
+    const numActiveTodos = state.todos.filter((t) => !t.completed).length;
+    toggleVisible(domElements.main, hasTodos);
+    toggleVisible(domElements.footer, hasTodos);
+    toggleVisible(domElements.clearBtn, hasCompletedTodos);
+    domElements.toggleAllBtn.checked = isAllTodosCompleted;
+    replaceHTML(domElements.count, `
+			<strong>${numActiveTodos}</strong>
+			${numActiveTodos === 1 ? "item" : "items"} left
+        `);
+    domElements.filterLinks.forEach((el) => {
+        if (el.matches(`[href="#/${state.page}"]`)) {
+            el.classList.add("selected");
+        }
+        else {
+            el.classList.remove("selected");
+        }
+    });
+}
+function dispatch(message, payload) {
+    const updateEvent = new CustomEvent('dispatch', { detail: { message: message, payload: payload } });
+    document.dispatchEvent(updateEvent);
+}
+export function init(el) {
+    const domElements = {
+        toggleAllBtn: el.querySelector('[data-todo="toggle-all"]'),
+        input: el.querySelector('[data-todo="new"]'),
+        clearBtn: el.querySelector('[data-todo="clear-completed"]'),
+        list: el.querySelector('[data-todo="list"]'),
+        main: el.querySelector('[data-todo="main"]'),
+        footer: el.querySelector('[data-todo="footer"]'),
+        count: el.querySelector('[data-todo="count"]'),
+        filterLinks: el.querySelectorAll(`[data-todo="filters"] a`),
+    };
+    let state = generateDefaultState();
+    document.addEventListener('dispatch', ((event) => {
+        const message = event.detail.message;
+        const payload = event.detail.payload;
+        state = update(state, message, payload);
+        console.log('state Listener', state);
+    }));
+    domElements.input.addEventListener("keyup", (e) => {
+        const value = e.target.value;
+        if (e.key === "Enter" && value.length) {
+            dispatch(Message.AddTodo, { todoTitle: value });
+            console.log('value', value);
+            domElements.input.value = '';
+        }
+    });
+    render(state, domElements);
+}
+init(document.body);
 //# sourceMappingURL=app.js.map
--- js/app.ts
+++ js/app.ts
@@ -1,8 +1,15 @@
-// import { delegate, getURLHash, insertHTML, replaceHTML } from "./helpers.js";
+import { delegate, getURLHash, insertHTML, replaceHTML } from "./helpers.js";
 // import { TodoStore, Todo } from "./store.js";
 
 interface DOMElements {
-	[index: string]: HTMLElement | HTMLInputElement;
+	input: HTMLInputElement;
+	toggleAllBtn: HTMLInputElement;
+	clearBtn: HTMLElement;
+	list: HTMLElement;
+	main: HTMLElement;
+	footer: HTMLElement;
+	count: HTMLElement;
+	filterLinks: NodeListOf<HTMLElement>;
 }
 
 // STATE
@@ -18,7 +25,7 @@ export enum Page {
 }
 export interface State {
 	todos: Array<Todo>;
-    idLedger:number;
+	idLedger: number;
 	page: Page;
 }
 
@@ -40,12 +47,11 @@ export interface Payload {
 export function generateDefaultState(): State {
 	return {
 		todos: [],
-        idLedger:1,
+		idLedger: 1,
 		page: Page.All,
 	};
 }
 
-let state = generateDefaultState();
 export function update(state: State, message: Message, payload: Payload): State {
 	const id = payload?.todoID ?? 0;
 	const title = payload?.todoTitle ?? "";
@@ -61,7 +67,7 @@ export function update(state: State, message: Message, payload: Payload): State 
 			id: state.idLedger || 1,
 		};
 		state.todos.push(todo);
-        state.idLedger += 1;
+		state.idLedger += 1;
 	}
 
 	if (message === Message.RemoveTodo) {
@@ -99,7 +105,95 @@ export function update(state: State, message: Message, payload: Payload): State 
 	return updated;
 }
 
-
 // VIEW
+function toggleVisible(el: HTMLElement, show: boolean) {
+	el.style.display = show ? "block" : "none";
+}
 
+export function render(state: State, domElements: DOMElements): void {
+	const hasTodos = !!state.todos.length;
+	const hasCompletedTodos = state.todos.some((t) => t.completed);
+	const isAllTodosCompleted = state.todos.every((todo) => todo.completed);
+	const numActiveTodos = state.todos.filter((t) => !t.completed).length;
+
+	// set state on pre rendered html
+	toggleVisible(domElements.main, hasTodos);
+	toggleVisible(domElements.footer, hasTodos);
+	toggleVisible(domElements.clearBtn, hasCompletedTodos);
+	domElements.toggleAllBtn.checked = isAllTodosCompleted;
+	replaceHTML(
+		domElements.count,
+		`
+			<strong>${numActiveTodos}</strong>
+			${numActiveTodos === 1 ? "item" : "items"} left
+        `
+	);
+
+	domElements.filterLinks.forEach((el) => {
+		if (el.matches(`[href="#/${state.page}"]`)) {
+			el.classList.add("selected");
+		} else {
+			el.classList.remove("selected");
+		}
+	});
+
+	// render dynamic html
+	//
+	//attach event handlers to dynamic html
+}
+interface Dispatch {
+        message:Message; 
+        payload:Payload;
+}
+function dispatch(message: Message, payload: Payload ){
+
+    const updateEvent = new CustomEvent<Dispatch>('dispatch', {detail:{ message: message, payload: payload }});
+    document.dispatchEvent(updateEvent);
+} 
+
+export function init(el: HTMLElement): void {
+	// build the dom elements needed for dom manipulation of pre rendered html
+	const domElements: DOMElements = {
+		toggleAllBtn: el.querySelector('[data-todo="toggle-all"]') as HTMLInputElement,
+		input: el.querySelector('[data-todo="new"]') as HTMLInputElement,
+		clearBtn: el.querySelector('[data-todo="clear-completed"]') as HTMLElement,
+		list: el.querySelector('[data-todo="list"]') as HTMLElement,
+		main: el.querySelector('[data-todo="main"]') as HTMLElement,
+		footer: el.querySelector('[data-todo="footer"]') as HTMLElement,
+		count: el.querySelector('[data-todo="count"]') as HTMLElement,
+		filterLinks: el.querySelectorAll(`[data-todo="filters"] a`) as NodeListOf<HTMLElement>,
+	};
+    	// get the page state
+    //
+
+	let state = generateDefaultState();
+    //  CustomEvent for updating the state
+
+    document.addEventListener('dispatch',((event: CustomEvent<Dispatch>)  => {
+        const message = event.detail.message as Message;
+        const payload = event.detail.payload as Payload;
+        state = update(state, message, payload);
+        console.log('state Listener' ,state);
+    }) as EventListener);
+
+
+    
+    
+	// set up the event listeners on the pre rended state
+	domElements.input.addEventListener("keyup", (e: KeyboardEvent) => {
+        const value = (e.target as HTMLInputElement).value; 
+		if (e.key === "Enter" && value.length) {
+            dispatch(Message.AddTodo, { todoTitle: value })
+            console.log('value' ,value);
+            domElements.input.value = '';
+		}
+	});
+
+	//
+
+	// render the view
+	render(state, domElements);
+	// set up global update event
+}
+init(document.body);
 
